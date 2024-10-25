ad4386ed8931737073fc06dc1e694225a176ca40
thisismavver <mavver@icloud.com>
2024-10-25 20:24:06 +0300

    da

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     # Создаем клавиатуру
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton("Нажми меня!", web_app=WebAppInfo('https://thisismavver.github.io/server-site/'))
+    keyboard = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton("Нажми меня!", web_app=WebAppInfo('https://thisismavver.github.io/server-site/'))
     keyboard.add(button)
 
     bot.send_message(message.chat.id, "Привет! Я простой бот. Нажми на кнопку ниже:", reply_markup=keyboard)
@@ -22,6 +22,12 @@ def button_click(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, f"Вы написали: {message.text}")
+
+@bot.message_handler(content_types=['web_app_data'])
+def web_app(message: types.Message):
+    a = message.web_app_data
+    bot.reply_to(message, a)
+
 
 if __name__ == '__main__':
     bot.polling()
